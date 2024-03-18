d86e3a56345d9500c28e23588235f7fe28c232b3
its144prelationship <pastalonely@gmail.com>
2024-03-18 16:57:39 +0700

    midterm example inclass

---
+++ midterm-project/example/main.tf
@@ -0,0 +1,19 @@
+terraform {
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "~> 4.16"
+        }
+    }
+    required_version = ">= 1.2.0"
+}
+provider "aws" {
+    region = "us-east-2"
+}
+resource "aws_instance" "app_server" {
+    ami = "ami-02ca28e7c7b8f8be1"
+    instance_type = "t2.micro"
+    tags = {
+        Name = "ExampleAppServerInstance"
+    }
+}
